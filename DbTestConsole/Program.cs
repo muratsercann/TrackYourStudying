f38c9622c1f7684d379e55e5b094386f8220542b
muratsercann <muratsercann@gmail.com>
2023-08-28 23:04:48 +0300

    a

--- DbTestConsole/Program.cs
+++ DbTestConsole/Program.cs
@@ -10,7 +10,7 @@ internal class Program
     private static void Main(string[] args)
     {
         //CreateSubjects();
-        CreateTopics();
+        //CreateTopics();
 
     }
     /// <summary>
